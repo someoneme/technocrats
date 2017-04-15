47e9dba64c17d7f6161e95a0d7d52985a76d95b5
Hardik Ahuja <hardikahuja9999@gmail.com>
2017-04-15 23:14:17 +0530

    Update in rotatry encoder

--- rotary_encoder/rotary_encoder.ino
+++ rotary_encoder/rotary_encoder.ino
@@ -1,25 +1,29 @@
 int counter = 0;
 int pin_a=0;
 int pin_b=0;
+int last_stage;
+int state;
 void setup(){
   pinMode(5,OUTPUT);
-  pinMode(6,OUTPUT);  
+  pinMode(6,OUTPUT); 
   Serial.begin(9600);
-  
+ 
   int pin_a = digitalRead(5);
   int pin_b = digitalRead(6);
 }
 
 void loop(){
-  if((pin_a==1)&&(pin_b==1)){
-    counter++;  
-  }  
-  if((pin_a==0)&&(pin_b==0)){
-    counter++;
+  pin_a = digitalRead(5);
+  pin_b = digitalRead(6);
+  if(pin_a!=last_stage){
+    if(pin_b!=pin_a){
+      counter++;
+    } 
+    else{
+      counter--; 
+    }
+    Serial.println(counter);
   }
-  
-  if(counter==15 ){
-    Serial.println("yes");
-  }
+ 
+  last_stage = pin_a;
 }
-
